b515acf66c0d67d292207a9fc7bcf9ce7e5f5376
erichu100 <erichu100@outlook.com>
2018-02-02 21:39:43 -0800

    Added NotCat Code

---
+++ Robot2/src/main/java/org/firstinspires/ftc/Robot2/DogeConfiguration.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.Robot2;
+
+/**
+ * Created by Eric on 2/2/2018.
+ */
+
+public class DogeConfiguration {
+    public static final String MainMotorName = "MainMotor";
+    public static final String TailServoName = "TailServo";
+    public static final String MouthServoName = "MouthServo";
+}
