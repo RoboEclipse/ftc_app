b26124c58c4773eb08791d219fb3a34ae53b1f8f
Catmeow <erichu100@outlook.com>
2018-09-16 15:01:14 -0700

    Merge remote-tracking branch 'upstream/master'
                                                # Conflicts:
                                                #	FtcRobotController/build.gradle
                                                #	FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorDigitalTouch.java
                                                #	build.common.gradle
                                                #	gradle/wrapper/gradle-wrapper.properties

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorDigitalTouch.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorDigitalTouch.java
@@ -29,11 +29,8 @@
 
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
-<<<<<<< HEAD
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-=======
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
->>>>>>> upstream/master
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
@@ -47,12 +44,9 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
  */
-<<<<<<< HEAD
-@Autonomous(name = "Sensor: Digital touch", group = "Sensor")
-=======
+
 @TeleOp(name = "Sensor: Digital touch", group = "Sensor")
 @Disabled
->>>>>>> upstream/master
 public class SensorDigitalTouch extends LinearOpMode {
     /**
      * The REV Robotics Touch Sensor
