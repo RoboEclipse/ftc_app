f5d8dd753bf28d41f8f11895f1beee370eafd1af
ethan <ethanh8093@gmail.com>
2018-12-30 18:45:03 -0800

    Added the double extendor motor to the Rover Rackus configuration.

--- Robot1/src/main/java/org/firstinspires/ftc/Robot2/RoverRackusConfigurationjava.java
+++ Robot1/src/main/java/org/firstinspires/ftc/Robot2/RoverRackusConfigurationjava.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.Robot2;
 
 public class RoverRackusConfigurationjava {
+    // Motor Names
     public static final String LeftFrontMotorName = "leftf_motor";
     public static final String RightFrontMotorName = "rightf_motor";
     public static final String LeftRearMotorName = "leftb_motor";
     public static final String RightRearMotorName = "rightb_motor";
     public static final String LeadScrewMotorName = "leadScrewMotor";
     public static final String doubleExtenderMotorName = "doubleExtenderMotor";
-    public static final String basketDumpingMechanism = "basketDumpingMotor";
-    
+    public static final String basketDumpingMechanismMotorName = "basketDumpingMotor";
+    public static final String intakeMchanismMotorName = "collectorMotor";
+    // Servo Names
+    public static final String collectionBasket = "cBasketMotor";
+
 }
