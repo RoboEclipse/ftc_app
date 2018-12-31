f396ecbf386f2e4d8c5c579177c139c17cd85386
ethan <ethanh8093@gmail.com>
2018-12-30 17:39:09 -0800

    Added the double extendor motor to the Rover Rackus configuration.

--- Robot1/src/main/java/org/firstinspires/ftc/Robot2/RoverRackusClass.java
+++ Robot1/src/main/java/org/firstinspires/ftc/Robot2/RoverRackusClass.java
@@ -1,10 +1,16 @@
 package org.firstinspires.ftc.Robot2;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class RoverRackusClass {
+    private DcMotor cMotor, screwUpPower;
+    private DigitalChannel elevatorLimitSwitch;
+
     public void getHorizontalAngle() {
     
     }
@@ -13,5 +19,23 @@ public class RoverRackusClass {
     }
 
     public void elevatorServoDrive(double elevatorServoPosition) {
+    }
+    public void cMotorDrive(double power)
+    {
+        cMotor.setPower(power);
+    }
+    public void leadScrewDrive(double power)
+    {
+        screwUpPower.setPower(power);
+    }
+    public void extendLeadScrew(double runtime){
+        ElapsedTime time = new ElapsedTime();
+        screwUpPower.setPower(1);
+        while(elevatorLimitSwitch.getState()==true){
+            if(time.seconds() > runtime){
+                break;
+            }
+        }
+        screwUpPower.setPower(0);
     }
 }
