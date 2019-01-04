6e0291bae3b550709b7292baf23cd71627c9333b
ethan <ethanh8093@gmail.com>
2019-01-03 19:49:36 -0800

    我先加了一些新东西。我会马上处理掉。

--- Robot2/src/main/java/org/firstinspires/ftc/Robot2/RoverRackusTeleOp.java
+++ Robot2/src/main/java/org/firstinspires/ftc/Robot2/RoverRackusTeleOp.java
@@ -2,22 +2,45 @@ package org.firstinspires.ftc.Robot2;
 
 import android.util.Log;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
 public abstract class RoverRackusTeleOp extends OpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    RoverRackusClass myRobot = new RoverRackusClass();
-    double theta = 0.0, v_theta = 0.0, v_rotation = 0.0;
-    double elevatorServoPosition = 1;
-    double speedMultiplier = 1;
-    double tokenServoPosition = 0;
-    double collectorServoPower = 0.5;
-    double leadScrewPower = 0;
-    boolean cFlipCheck = false;
-    private String elevatorDistance;
-    private String cFlipEncoder;
-    private String cFlipPower;
+    public RoverRackusClass myRobot = new RoverRackusClass();
+    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+    private static final String VUFORIA_KEY = "AcszqaP/////AAABmQPh2+SHAkBwsXAKy4LdjLEctzmZIadppxAnjn5ubFiLREbyOyViDtItmB2qAtRfbfJ1GRhhAXPEl992rkY/XW50xxWEVrQ+FGKMC1m6PDC1ropQyBiufMSvx81nz+XF6eSHp6Ct2rrT4YutN9a81bcvGVNA+4EfTu98lzP2HrPUiv0SMlQVq+ze6Fw107r8e7ULdv7dbdfxVtS0X+H4toGS+gxJFyWlgcdHmchQ++I7n8RdaBqoVgItHzjZDDo3lMbPkHIMwsTbWlBYJDBoNGMiFnIBUm1t0J6Yu45dldLZ8eeTPn7R9M9MkwYyWLmAr8Ijs0bxmqDuY4NgvmDqeCIzbfyH7uEximJiwZaGu58u";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+    private DcMotor lf, lr, rf, rr, leadScrew, cmotor, cflip, emotor;
+    private CRServo exservo, exservoback;
+    private DistanceSensor leftDistanceSensor, rightDistanceSensor, elevatorDistanceSensor, extenderDistanceSensor;
+    private ColorSensor colorSensor;
+    private Servo elevatorServo, markerServo;
+    private Telemetry telemetry;
+    private com.qualcomm.robotcore.hardware.HardwareMap HardwareMap;
+    private BNO055IMU imu;
+    private Orientation angles;
+    private DigitalChannel elevatorLimitSwitch;
+    public static final int ENCODERS_CLOSE_ENOUGH = 10;
+    int TICKS_PER_ROTATION = 1120;
+    public int TICKS_PER_INCH = (int)(1120/(6*Math.PI));
+    public int TICKS_PER_CENTIMETER =(int)(TICKS_PER_INCH*2.54);
+    public int leadScrewTime=5000;
+    RoverRackusConfiguration config = new RoverRackusConfiguration();
 
 
     /*
@@ -78,15 +101,11 @@ public abstract class RoverRackusTeleOp extends OpMode {
         v_theta = Math.sqrt(lx * lx + ly * ly);
         v_rotation = gamepad1.right_stick_x;
 
-        /*
-
-        This part is having some errors due to myRobot not ddefined
-
         myRobot.drive(theta,  speedMultiplier*0.6*v_theta, 0.5*v_rotation); //move robot
 
-        */
-        /*
-        Working on this
+
+
+        //Working on this
 
         //Lead Screw Controls
         if(gamepad1.left_bumper){
@@ -110,7 +129,7 @@ public abstract class RoverRackusTeleOp extends OpMode {
         }
 
         myRobot.leadScrewDrive(leadScrewPower);
-        */
+
 
         //Elevator Motor Controls
         double elevatorPower = 1;
