6f1f18705fc58adf9cf9de440eeb9a0137a57a7b
Catmeow <erichu100@outlook.com>
2019-08-30 21:16:14 -0700

    Got Dashboard working with SKYSTONEConstants

--- Robot1/src/main/java/org/firstinspires/ftc/Robot1/SKYSTONEConstants.java
+++ Robot1/src/main/java/org/firstinspires/ftc/Robot1/SKYSTONEConstants.java
@@ -4,9 +4,9 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class SKYSTONEConstants {
-    double TICKS_PER_ROTATION = 103.6;
-    double GEAR_RATIO = 20;
-    double TICKS_PER_WHEEL_ROTATION = TICKS_PER_ROTATION*GEAR_RATIO;
-    double TICKS_PER_INCH = TICKS_PER_WHEEL_ROTATION/(4*Math.PI);
-    double testDistance = 5;
+    public static double TICKS_PER_ROTATION = 103.6;
+    public static double GEAR_RATIO = 20;
+    public static double TICKS_PER_WHEEL_ROTATION = TICKS_PER_ROTATION*GEAR_RATIO;
+    public static double TICKS_PER_INCH = TICKS_PER_WHEEL_ROTATION/(4*Math.PI);
+    public static double testDistance = 5;
 }
